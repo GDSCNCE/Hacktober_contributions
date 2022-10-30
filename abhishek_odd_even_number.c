e06bfc458c1228adf75b4962cd2b0cdee7e6f12b
Abhishek anand <110963639+abhikanand31@users.noreply.github.com>
2022-10-30 12:43:18 +0530

    Create abhishek_odd_even_number.c

---
+++ abhishek_odd_even_number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter the value a",a);
+    scanf("%d",&a);
+    if(a % 2 == 0)
+        
+        {
+        printf("%d is even",a);
+        } 
+        
+    else{
+        printf("%d is odd",a);
+    };
+    return 0;
+}
